8671acb8a6579e55d32d1aecb2c89aebab6724a8
nfp13 <nikolas.plett@icloud.com>
2024-06-26 14:55:52 +0200

    Fixed database class data reading

--- RohrleitungsGenerator/database.cs
+++ RohrleitungsGenerator/database.cs
@@ -12,7 +12,14 @@ namespace ROhr2
         {
             //intitialziren
             _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            _GenerateTemplateFile();
             _OpenRefFile();
+
+            _GetFluidData();
+            //_GetNormrohreData();
+            //_GetWerkstoffData();
+
+            _CloseExcel();
         }
 
         public void _OpenRefFile()
@@ -24,7 +31,7 @@ namespace ROhr2
             _wsFluid = (Worksheet)_wb.Worksheets[3];
         }
 
-        public void CloseExcel()
+        private void _CloseExcel()
         {
             _wb.Close();
             _excelApp.Quit();
@@ -47,54 +54,53 @@ namespace ROhr2
             }
         }
 
-        public void GetNormrohreData(double holeDia)
+        private void _GetNormrohreData()
         {
-            int MaxRowN = _wsNormrohre.Rows.Count;
+            int RowCount = _wsNormrohre.UsedRange.Rows.Count;
             int i = 2;
-            double readExcelDia = 0.0;
 
-            while (i < MaxRowN && readExcelDia < holeDia)
+            while (i < RowCount + 1)
             {
-                i++;
                 double Außenradius = (double)(_wsNormrohre.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range).Value;
                 double Wandstärke = (double)(_wsNormrohre.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range).Value;
                 double Biegeradius = (double)(_wsNormrohre.Cells[i, 3] as Microsoft.Office.Interop.Excel.Range).Value;
                 Normrohre.Add(new Normrohr(Außenradius, Wandstärke, Biegeradius));
+                i++;
             }
                       
         }
 
-        public void GetWerkstoffData(double holeDia)
+        private void _GetWerkstoffData()
         {
-            int MaxRowN = _wsWerkstoff.Rows.Count;
-            int i = 2;
-            double readExcelDia = 0.0;
+            int RowCount = _wsWerkstoff.UsedRange.Rows.Count;
+            int i = 3;
 
-            while (i < MaxRowN && readExcelDia < holeDia)
+            while (i < RowCount + 1)
             {
-                i++;
-                double Emodul = (double)(_wsWerkstoff.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range).Value;
-                double Wärmeausdehnung = (double)(_wsWerkstoff.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range).Value;
-                double Mindestzugfestigkeit = (double)(_wsWerkstoff.Cells[i, 3] as Microsoft.Office.Interop.Excel.Range).Value;
-                double Dichte = (double)(_wsWerkstoff.Cells[i, 4] as Microsoft.Office.Interop.Excel.Range).Value;
-                double Schubmodul = (double)(_wsWerkstoff.Cells[i, 5] as Microsoft.Office.Interop.Excel.Range).Value;
-                Werkstoffe.Add(new Werkstoff(Emodul, Wärmeausdehnung, Mindestzugfestigkeit,Dichte,Schubmodul));
+                string name = (string)(_wsWerkstoff.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Emodul = (double)(_wsWerkstoff.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Wärmeausdehnung = (double)(_wsWerkstoff.Cells[i, 3] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Mindestzugfestigkeit = (double)(_wsWerkstoff.Cells[i, 4] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Dichte = (double)(_wsWerkstoff.Cells[i, 5] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Schubmodul = (double)(_wsWerkstoff.Cells[i, 6] as Microsoft.Office.Interop.Excel.Range).Value;
+                Werkstoffe.Add(new Werkstoff(Emodul, Wärmeausdehnung, Mindestzugfestigkeit,Dichte,Schubmodul, name));
                 //ggf. i bei 2 anfagen?
+                i++;
             }
 
         }
 
-        public void GetFluidData(double holeDia)
+        private void _GetFluidData()
         {
-            int MaxRowN = _wsFluid.Rows.Count;
-            int i = 2;
-            double readExcelDia = 0.0;
+            int RowCount = _wsFluid.UsedRange.Rows.Count;
+            int i = 3;
 
-            while (i < MaxRowN && readExcelDia < holeDia)
+            while (i < RowCount + 1)
             {
+                string name = (string)(_wsFluid.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range).Value;
+                double Dichte = (double)(_wsFluid.Cells[i, 2] as Microsoft.Office.Interop.Excel.Range).Value;
+                Fluids.Add(new Fluid(name, Dichte)) ;
                 i++;
-                double Dichte = (double)(_wsFluid.Cells[i, 1] as Microsoft.Office.Interop.Excel.Range).Value;
-                Fluids.Add(new Fluid(Dichte));
                 //Name noch mit rein?
             }
 
@@ -120,9 +126,9 @@ namespace ROhr2
             Wandstärke = wandstärke;
             Biegeradius = biegeradius;
         }
-        public double Außenradius;
-        public double Wandstärke;
-        public double Biegeradius;
+        public double Außenradius { get; set; }
+        public double Wandstärke { get; set; }
+        public double Biegeradius { get; set; }
     }
 
     public class Werkstoff
@@ -136,12 +142,12 @@ namespace ROhr2
             Schubmodul = schubmodul;
             Name = name;
         }
-        public double Emodul;
-        public double Wärmeausdehnung;
-        public double Mindestzugfestigkeit;
-        public double Dichte;
-        public double Schubmodul;
-        public string Name;
+        public double Emodul { get; set; }
+        public double Wärmeausdehnung { get; set; }
+        public double Mindestzugfestigkeit { get; set; }
+        public double Dichte { get; set; }
+        public double Schubmodul { get; set; }
+        public string Name { get; set; }
     }
 
     public class Fluid
@@ -151,7 +157,7 @@ namespace ROhr2
             Name = name;
             Dichte = dichte;
         }
-        public string Name;
-        public double Dichte;
+        public string Name { get; set;}
+        public double Dichte { get; set; }
     }
 }
